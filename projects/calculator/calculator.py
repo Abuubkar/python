269e299185693c61f2b23994c883be0425ebea7a
Abubakar548 <bsef17m548@pucit.edu.pk>
2020-02-09 23:35:45 +0500

    menu completed

--- projects/calculator/calculator.py
+++ projects/calculator/calculator.py
@@ -3,7 +3,8 @@ try:
     class calculator():
 
         def __init__(self, value1=0, value2=0):
-            self.__value1, self.__value2 = float(value1), float(value2)
+            self.set_value1(value1)
+            self.set_value2(value2)
             self.__result = 0
 
         def add(self):
@@ -22,12 +23,18 @@ try:
             self.__result = self.__value1 / self.__value2
             self.print()
 
+        def set_value1(self, value):
+            self.__value1 = float(value)
+
+        def set_value2(self, value):
+            self.__value2 = float(value)
+
         def print(self):
-            print("Your answer is "+self.__result.__str__())
+            print("\nYour answer is "+self.__result.__str__())
 
         def print_value(self):
-            print("Value1 = " + str(self.__value1) +
-                  "\nValue2 = " + str(self.__value2))
+            print("\n1st Value = " + str(self.__value1) +
+                  "\n2nd Value = " + str(self.__value2))
 
 except ValueError as ve:
     print(ve.__class__.__name__ + " : Value not Numeric")
--- projects/calculator/menu.py
+++ projects/calculator/menu.py
@@ -1,15 +1,47 @@
 from calculator import *
+import os
+# import time
+# time.sleep(3)
 
-print("Welcome to Calculatro!")
-print("======================")
-print("\nFor calculation:")
+
+def header():
+    print("Welcome to Calculatro!")
+    print("======================")
+    print("For calculation:")
+
+
+header()
+
+# Creation & Instantiation
 menu = 0
-while(menu != '8'):
-    cal = calculator(input("Enter numbers: \n"),
-                     input("Enter numbers: \n"))
-    print('\x1bc')
-    print("1.Addition\t2.Subtraction\n3.Multiplication\t4.Division\n5.Print last result\t6.Change Values")
+cal = calculator(input("Enter 1st Value: "),
+                 input("Enter 2nd Value: "))
+
+# Processing
+while(True):
+    print("\n1.Addition\t2.Subtraction\n3.Multiplication\t")
+    print("4.Division\n5.Print Last Result\t6.Change Values")
     print("7.Print Values\t8.Exit")
+
     menu = input("Choose on of above option: ")
+
     if(menu == "1"):
         cal.add()
+    if(menu == "2"):
+        cal.sub()
+    if(menu == "3"):
+        cal.mul()
+    if(menu == "4"):
+        cal.div()
+    if(menu == "5"):
+        cal.print()
+    if(menu == "6"):
+        cal.set_value1(input("Enter 1st value: "))
+        cal.set_value2(input("Enter 2nd value: "))
+    if(menu == "7"):
+        cal.print_value()
+    if(menu == "8"):
+        break
+
+    os.system("pause")
+    os.system("cls")
